6ceb7267b8c1971b31cf12e06b68751b1cfb81c4
MaxT-21 <mtubito21@gmail.com>
2021-05-13 16:16:06 +0200

    Correzione del parse

--- progettoMAP/src/main/java/com/mycompany/progettomap/parser/Parser.java
+++ progettoMAP/src/main/java/com/mycompany/progettomap/parser/Parser.java
@@ -5,10 +5,35 @@
  */
 package com.mycompany.progettomap.parser;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author mtubi
  */
 public class Parser {
+    public ParserOutput parse(String comandoUtente){
+        ParserOutput comando=null;
+        comandoUtente=comandoUtente.toLowerCase();
+        comandoUtente=comandoUtente.replaceAll("[.!£$%&/]+", "");//Pulizia del comando di caratteri di punteggiatura
+        String[] paroleComando=comandoUtente.split("\\s+|'");//Divido il comando in base agli spazi in modo da esaminare parola per parola
+        if(paroleComando.length>0){//controllo che dopo le varie pulizie la stringa non sia vuota
+            comando=esaminaParole(paroleComando);
+        }
+        return comando;
+    }
     
+    public ParserOutput esaminaParole(String[] paroleComando){
+        ParserOutput comando=null;
+        List<String> articoli= new ArrayList();
+        for(int i=0; i<paroleComando.length;i++){
+            if(articoli.indexOf(paroleComando[i])==-1){
+                
+            }
+            i++;
+        }
+        return comando;
+    }
+            
 }
--- progettoMAP/src/main/java/com/mycompany/progettomap/parser/ParserOutput.java
+++ progettoMAP/src/main/java/com/mycompany/progettomap/parser/ParserOutput.java
@@ -5,28 +5,12 @@
  */
 package com.mycompany.progettomap.parser;
 
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  *
  * @author mtubi
  */
 public class ParserOutput {
-    public ParserOutput parse(String comandoUtente){
-        ParserOutput comando=null;
-        puliziaStringa(comandoUtente);
-        
-        
-        return comando;
-    }
     
-    public void puliziaStringa(String comandoUtente){
-        List<String> articoli = new ArrayList();
-        comandoUtente=comandoUtente.replaceAll("[.!£$%&/]+", "");//Pulizia del comando di caratteri di punteggiatura
-        for(String articolo : articoli){
-             comandoUtente=comandoUtente.replace(articolo, "");//cancellazione di tutti i tipi di articoli all'interno del comando
-        }
     }
-}
 
