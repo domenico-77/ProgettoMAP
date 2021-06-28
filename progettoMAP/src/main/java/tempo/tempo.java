3380a8d092af733a803f9b40604402166883b90a
domenico-77 <d.vicenti1@studenti.uniba.com>
2021-06-28 10:17:16 +0200

    Creata la classe per calcolare il tempo

---
+++ progettoMAP/src/main/java/tempo/tempo.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tempo;
+
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ *
+ * @author domen
+ */
+public class tempo {
+
+    static final int MAX_SEC = 59;
+    static final int MAX_MIN = 59;
+    static int secondi = 0;
+    static int minuti = 0;
+    static int ore = 0;
+    static Timer timer;
+
+    TimerTask timerTask = new TimerTask() {
+        
+
+        @Override
+        public void run() {
+            if (secondi == MAX_SEC) {
+                if (minuti == MAX_MIN) {
+                    secondi = 0;
+                    minuti = 0;
+                    ore++;
+                } else {
+                    secondi = 0;
+                    minuti++;
+                }
+            } else {
+                secondi++;
+            }
+        }
+    };
+
+    //creazione thread che stampa i valori
+    Thread t = new Thread(() -> {
+        
+        
+        while (true) {
+            try {
+                Scanner scanner = new Scanner(new InputStreamReader(System.in));
+                String x = scanner.nextLine();
+                if ("tempo".equals(x)) {
+                    System.out.println("Tempo passato: " + ore + ":" + minuti + ":" + secondi);
+                }
+                //qua
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+    });
+
+    timer  = new Timer("MyTimer");
+
+    timer.scheduleAtFixedRate (timerTask, 10, 1000);//start timer
+}
