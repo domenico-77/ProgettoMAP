4ad45fbfe8dddf2b4297a47ba19a233b9898ec60
domenico-77 <d.vicenti1@studenti.uniba.com>
2021-07-12 20:10:22 +0200

    a

---
+++ progettoMAP/src/main/java/swing/mainSwing.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swing;
+
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author domen
+ */
+public class mainSwing {
+    private JPanel menuInizio;
+    private JPanel giocoGui;
+    private JFrame frame = new JFrame();
+    
+    
+    public mainSwing(){
+        giocoGui = new giocoGui(this);
+        menuInizio = new menuInizio(this);
+        //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setContentPane(this.menuInizio);
+                frame.setPreferredSize(new Dimension(500, 400));
+               // frame.pack();
+                frame.setVisible(true);
+        
+    }
+
+    public JPanel getGiocoGui() {
+        return giocoGui;
+    }
+
+    public JPanel getMenuInizio() {
+        return menuInizio;
+    }
+    
+    public static void main(String args[])
+    {
+        EventQueue.invokeLater(new Runnable()
+        {
+            public void run()
+            {
+                mainSwing ms = new mainSwing();
+                ms.getFrame().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                ms.getFrame().pack();
+            }
+        });
+    }
+
+    public JFrame getFrame() {
+        return frame;
+    }
+    
+    
+}
